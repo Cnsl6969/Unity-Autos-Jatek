69e7319424728a4b9f949b19140e731b700e09f0
fecoka0325 <kovacsfeco0325@gmail.com>
2024-05-04 11:02:53 +0200

    szeretem_csirkét

---
+++ Assets/Scripts/CarMovementScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovementScript : MonoBehaviour
+{
+    public GameObject[] wheels;
+    public bool fourWheelDrive = true;
+    public float maxMotorTorque; // max motor nyomaték
+    public float maxSteeringAngle; // max kanyarodási szög
+    public float maxBrakeTorque; // max fék nyomaték / fék erő
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
